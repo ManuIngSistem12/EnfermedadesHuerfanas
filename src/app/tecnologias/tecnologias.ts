8f233bf8ee6a8fd2b8405383b09049a3d2c19fb5
Manuel <manuel.norenac@autonoma.edu.co>
2026-04-02 23:19:16 -0500

    feat: Se realizo y finalizo el apartado 'tecnologias' del proyecto

--- src/app/tecnologias/tecnologias.ts
+++ src/app/tecnologias/tecnologias.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, TemplateRef } from '@angular/core';
+import { NgTemplateOutlet, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tecnologias',
-  imports: [],
+  imports: [NgTemplateOutlet, NgIf],
   templateUrl: './tecnologias.html',
   styleUrl: './tecnologias.css',
 })
 export class Tecnologias {
 
+  modalActivo: boolean = false;
+  templateActivo: TemplateRef<any> | null = null;
+
+  abrirModal(template: TemplateRef<any>): void {
+    this.templateActivo = template;
+    setTimeout(() => this.modalActivo = true, 10);
+  }
+
+  cerrarModal(): void {
+    this.modalActivo = false;
+    setTimeout(() => this.templateActivo = null, 450);
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    if (this.modalActivo) this.cerrarModal();
+  }
 }
+
