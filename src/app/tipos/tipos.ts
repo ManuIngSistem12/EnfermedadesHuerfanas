7cfd85c7c6a2776f2fbfc6e0ade769e6fe73b0d0
Manuel <manuel.norenac@autonoma.edu.co>
2026-04-02 22:27:17 -0500

    feat: Se realizo y finalizo el apartado 'tipos' del proyecto

--- src/app/tipos/tipos.ts
+++ src/app/tipos/tipos.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-tipos',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './tipos.html',
   styleUrl: './tipos.css',
 })
